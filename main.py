27e1b1311f17d42821d7640ee111c2eb452b97e2
rjanamsetty <janamsetty@gmail.com>
2022-11-26 18:26:51 -0500

    added command line args to server

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import csv
+import sys
 import time
 
-from flask import Flask, request
-import numpy as np
 import cv2
+import numpy as np
+from flask import Flask, request
 
 import frame_handler
 
@@ -12,7 +13,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Server up and running"
+    return "Server up and running with {!r}".format(app.config.get('image'))
 
 
 @app.route('/frame', methods=['PUT'])
@@ -22,7 +23,7 @@ def frame():
     f = cv2.imdecode(ppm_arr, cv2.IMREAD_COLOR)
 
     # Perform feature extraction
-    ref = cv2.imread('ballast.png')
+    ref = cv2.imread(app.config.get('image') + '.png')
     ref_img = cv2.resize(ref, (200, 200))
 
     try:
@@ -44,5 +45,8 @@ def store():
     writer.writerow(to_store)
     return "Success"
 
+
 if __name__ == "__main__":
-    app.run(host="192.168.0.13", port=8080, debug=True)
+    app.config['image'] = sys.argv[1]
+    # app.run(host="192.168.0.13", port=8080, debug=True)
+    app.run(host="127.0.0.1", port=8080, debug=True)
